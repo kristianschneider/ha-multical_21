0d90c89d0ef7dbdc49d315949468b45a9b5b0967
Kristian Schneider <khs@thamesconsulting.com>
2024-11-17 15:08:41 +0100

    .

--- custom_components/multical_21/__init__.py
+++ custom_components/multical_21/__init__.py
@@ -74,11 +74,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    )
+
+    # for platform in PLATFORMS:
+    #     if entry.options.get(platform, True):
+    #         hass.async_create_task(
+    #             hass.config_entries. async_forward_entry_setups(entry, platform)
+    #         )
 
     await coordinator.async_config_entry_first_refresh()
 
@@ -89,17 +93,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload this config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         del hass.data[DOMAIN][entry.entry_id]
+
     return unload_ok
 
 
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+async def async_reload_entry(hass, entry):
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 class KamstrupUpdateCoordinator(DataUpdateCoordinator):
