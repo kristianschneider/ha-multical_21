51499334754c54a5fbe536dcc9bc8b6eb7ac1ed5
mallewski <114901470+mallewski@users.noreply.github.com>
2026-05-22 08:49:22 +0200

    Bump version from 2.2.4 to 2.2.5

--- custom_components/multical_21/const.py
+++ custom_components/multical_21/const.py
@@ -5,7 +5,7 @@ from typing import Final
 # Base component constants
 NAME: Final = "multical 21"
 DOMAIN: Final = "multical_21"
-VERSION: Final = "2.2.4"
+VERSION: Final = "2.2.5"
 MODEL: Final = "Multical 21"
 MANUFACTURER: Final = "Kamstrup"
 ATTRIBUTION: Final = "Data provided by multical 21 meter"
